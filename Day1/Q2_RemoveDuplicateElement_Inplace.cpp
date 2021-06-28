91f0f4f84a1c6652b7f07e3b884ec2ebe70875a5
Sunny Goswami <sunny@pop-os.localdomain>
2021-06-28 23:07:13 +0530

    Date:June28,2021

---
+++ Day1/Q2_RemoveDuplicateElement_Inplace.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+       int i=0, j=0;
+       while(j < nums.size()){
+           if (nums[j] != val){
+               nums[i] = nums[j];
+               i++;
+           }
+           j++;
+       }
+       return i;
+    }
+};
+int main(){
+    int N; cin >> N;
+    vector<int> arr;
+    int ele;
+    for(int i=0; i<N; i++){
+        cin >> ele;
+        arr.push_back(ele);
+    }
+      
+    int key; cin >> key;
+    Solution obj; 
+    int k = obj.removeElement(arr, key);
+
+    cout << k <<" ";
+
+    for(int i=0; i<=k; i++){
+        cout << arr[i] << " ";
+    }
+}
